5be41746d07a73ecd337c77a6b4483b753d70fb9
Sonal Singh <sonal.singh.19993@gmail.com>
2021-04-16 01:29:23 +0530

    miatoll: arrow -> xtended
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- xtended_miatoll.mk
+++ xtended_miatoll.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The ArrowOS
+# Copyright (C) 2020 The Project-Xtended
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,23 +7,25 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Xtended stuff
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+XTENDED_BUILD_TYPE := OFFICIAL
+XTENDED_BUILD_MAINTAINER := SonalSingh
+XTENDED_BUILD_DONATE_URL := https://www.paypal.me/sonal18
+XTENDED_BUILD_SUPPORT_URL := https://t.me/arrow_xtended_miatoll
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := xtended_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Maintainer
-DEVICE_MAINTAINER := SonalSingh18
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
